d13552df0fdf1fc502531cfae63f71c00380bf2a
Salman Bukhari <salmanbukhari37@gmail.com>
2019-09-10 14:54:09 +0500

    server | package | updated

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname + "/dist"));
+
+app.listen(process.env.PORT || 3000);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
+console.log(`Port listening to ${process.env.PORT}`);
